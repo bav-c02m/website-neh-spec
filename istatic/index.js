b1f2cd32159af96995e239d045381710e264e31c
bav.c02m <bav.c02m@gmail.com>
2025-01-10 15:25:43 +0100

    scroll

--- istatic/index.js
+++ istatic/index.js
@@ -7,24 +7,19 @@
 // // Общее количество слайдов
 // const totalSlides = tagsSlide.length; 
 
-// // Получаем размеры контейнера .mid для учета padding и gap
-// const container = document.querySelector('.mid');
-// const containerWidth = container.clientWidth - (parseFloat(getComputedStyle(container).paddingLeft) + parseFloat(getComputedStyle(container).paddingRight));
-// const slideWidth = tagsSlide[0].offsetWidth + parseFloat(getComputedStyle(container).gap); // ширина слайда с учетом gap
-
 // // Следующий слайд
 // function showNextSlide() {
 //     // Переход (циклично)
 //     startPositionX = (startPositionX + 1) % totalSlides; 
     
-//     // Применяем смещение с учетом ширины контейнера и gap
-//     const offset = -(startPositionX * slideWidth); 
+//     // В процентах
+//     const offset = -startPositionX * 100; 
     
 //     // Применение смещения к каждому слайду
 //     tagsSlide.forEach(slide => {
-//         slide.style.transform = `translateX(${offset}px)`; 
+//         slide.style.transform = `translateX(${offset}%)`; 
 //     });
 // }
 
 // // Каждые 7 секунд
-// setInterval(showNextSlide, 7000);
+// setInterval(showNextSlide, 7000); 
