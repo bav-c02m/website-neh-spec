2f3f419e38c390b34be3b6f1650c6feaa78d0667
bav.c02m <bav.c02m@gmail.com>
2025-01-30 11:50:21 +0100

    qqq

--- ws-cs/static/js/21_logo-scroll.js
+++
@@ -1,6 +0,0 @@
-
-function logoScroll(event) {
-  const scrollCont = document.querySelector('.scroll-x');
-  const firstCont = document.getElementById('teacher');
-  scrollCont.scrollLeft = firstCont.offsetLeft;
-}
--- ws-cs/static/js/30_images-fullscreen.js
+++
@@ -1,5 +0,0 @@
-
-function zoomImg(event) {
-  const img = event.target;
-  img.classList.toggle("fullscreen")
-}
